5379bae6eb986bf705c2d591a68985730cf63aab
Kenneth Buck <kenneth@nestedsphere.com>
2015-03-15 15:27:45 -0400

    removed model injection from initializer

--- app/initializers/inject-map-service.js
+++ app/initializers/inject-map-service.js
@@ -1,6 +1,5 @@
 
 export function initialize(container, application) {
-  application.inject('model:point', 'map', 'service:map');
   application.inject('controller:index', 'map', 'service:map');
   application.inject('component:ui-map', 'map', 'service:map');
   application.inject('route:index', 'map', 'service:map');
