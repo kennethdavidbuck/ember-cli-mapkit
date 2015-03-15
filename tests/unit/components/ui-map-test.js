53eeb3658d23c361bbd55d5d2d6fe4f9db000471
Kenneth Buck <kenneth@nestedsphere.com>
2015-03-15 14:41:49 -0400

    fixed failing component test that required mock map service to be injected

--- tests/unit/components/ui-map-test.js
+++ tests/unit/components/ui-map-test.js
@@ -1,14 +1,27 @@
 import {
   moduleForComponent,
   test
-} from 'ember-qunit';
+  } from 'ember-qunit';
+
+import Ember from 'ember';
 
 moduleForComponent('ui-map', {
   // specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar']
+  needs: ['service:map'],
+
+  setup: function () {
+    this.subject().set('map', Ember.Object.create({
+      setup: function () {
+
+      },
+      teardown: function () {
+
+      }
+    }));
+  }
 });
 
-test('it renders', function(assert) {
+test('it renders', function (assert) {
   assert.expect(2);
 
   // creates the component instance
