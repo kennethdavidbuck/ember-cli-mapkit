0c05672949f3731c6e593e0f60508b592dace414
Kenneth Buck <kenneth@nestedsphere.com>
2015-05-03 21:01:42 -0400

    moved csp to config hook in index.js

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ module.exports = {
   config: function (env, baseConfig) {
     var ENV = {
       APP: {
+        contentSecurityPolicy: {
+          'default-src': "'none'",
+          'script-src':  "'self' 'unsafe-inline' 'unsafe-eval' *.googleapis.com maps.gstatic.com gist.github.com",
+          'font-src':    "'self' fonts.gstatic.com",
+          'connect-src': "'self'",
+          'img-src':     "'self' *.gstatic.com *.googleapis.com",
+          'style-src':   "'self' 'unsafe-inline' gist-assets.github.com fonts.googleapis.com",
+          'frame-src':   "ghbtns.com platform.twitter.com"
+        },
+
         MAPKIT: {
           KEY: null,
 
