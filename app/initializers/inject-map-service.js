8bde2d570314429af8ebd73c8a0ad464f058f78d
Kenneth Buck <kenneth@nestedsphere.com>
2015-05-10 20:19:56 -0400

    now injecting map service in all routes and controllers

--- app/initializers/inject-map-service.js
+++ app/initializers/inject-map-service.js
@@ -6,9 +6,9 @@ export function initialize(container, application) {
   container.register('google:marker-clusterer', MarkerClusterer, {instantiate:false});
 
   application.inject('service:map', 'googleApi', 'google:main');
-  application.inject('controller:index', 'map', 'service:map');
+  application.inject('controller', 'map', 'service:map');
   application.inject('component:ui-map', 'map', 'service:map');
-  application.inject('route:index', 'map', 'service:map');
+  application.inject('route', 'map', 'service:map');
 }
 
 export default {
