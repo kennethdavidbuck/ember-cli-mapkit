f2340946984a5b50ae1abbb18b972595f70dbc71
Kenneth Buck <kenneth@nestedsphere.com>
2015-05-29 19:16:53 -0400

    formatting

--- tests/unit/services/map-test.js
+++ tests/unit/services/map-test.js
@@ -1,16 +1,18 @@
 import {
   moduleFor,
   test
-} from 'ember-qunit';
+  } from 'ember-qunit';
 
 moduleFor('service:map', {
   needs: ['container'],
-  setup: function () { },
-  teardown: function () { }
+  setup: function () {
+  },
+  teardown: function () {
+  }
 });
 
 // Replace this with your real tests.
-test('it exists', function(assert) {
+test('it exists', function (assert) {
   var service = this.subject();
   assert.ok(service);
 });
