442b8a8eda10ed4701e9ae5c08fa5598a3d0cbe4
Kenneth Buck <kenneth@nestedsphere.com>
2015-09-27 15:03:38 -0400

    created ui-google-map component and tests

---
+++ addon/components/ui-google-map.js
@@ -0,0 +1,50 @@
+import Ember from 'ember';
+import layout from '../templates/components/ui-google-map';
+
+export default Ember.Component.extend({
+
+  layout: layout,
+
+  tagName: 'ui-google-map',
+
+  classNames: ['ui-map', 'ui-google-map'],
+
+  action: 'mapEvent',
+
+  markerAction: 'markerEvent',
+
+  readyAction: 'mapReady',
+
+  actionHandlers: ['readyAction', 'action', 'markerAction'],
+
+  /**
+   *
+   */
+  setup: Ember.on('didInsertElement', function () {
+    const props = this.getProperties('map', 'actionHandlers');
+
+    Ember.run.scheduleOnce('afterRender', this, function () {
+      props.actionHandlers.forEach((handler) => {
+        props.map.on(handler, this, function () {
+          this.sendAction.apply(this, [handler].concat([].slice.call(arguments)));
+        });
+      });
+
+      props.map.register(this);
+    });
+  }),
+
+  /**
+   *
+   */
+  teardown: Ember.on('willDestroyElement', function () {
+    const props = this.getProperties('map', 'actionHandlers');
+
+    props.actionHandlers.forEach((handler) => {
+      props.map.off(handler, this);
+    });
+
+    props.map.unregister(this);
+  })
+});
+
