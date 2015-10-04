c3dce74a4b55a855424f7952d26d11512d422ed2
Kenneth Buck <kenneth@nestedsphere.com>
2015-10-04 15:47:30 -0400

    formatting

--- tests/unit/components/ui-abstract-map-test.js
+++ tests/unit/components/ui-abstract-map-test.js
@@ -5,11 +5,9 @@ moduleForComponent('ui-abstract-map', {});
 test('it renders', function (assert) {
   assert.expect(2);
 
-  // creates the component instance
   const component = this.subject();
   assert.equal(component._state, 'preRender');
 
-  // renders the component to the page
   this.render();
   assert.equal(component._state, 'inDOM');
 });
@@ -23,4 +21,14 @@ test('sets own map facade when not present', function (assert) {
   this.render();
 
   assert.ok(component.get('mapFacade'), 'should have an instance of MapFacade');
+});
+
+test('initial marker map size is 0', function (assert) {
+  assert.expect(1);
+
+  const component = this.subject();
+
+  this.render();
+
+  assert.strictEqual(component.size(), 0, 'map size should be empty on initial render');
 });
