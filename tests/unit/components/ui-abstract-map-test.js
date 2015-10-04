4cbdd30bed5059523445a104a84170126bf51467
Kenneth Buck <kenneth@nestedsphere.com>
2015-10-04 15:43:07 -0400

    Added test for creation of mapFacade on abstract map component

--- tests/unit/components/ui-abstract-map-test.js
+++ tests/unit/components/ui-abstract-map-test.js
@@ -1,7 +1,4 @@
-import {
-  moduleForComponent,
-  test
-} from 'ember-qunit';
+import { moduleForComponent, test } from 'ember-qunit';
 
 moduleForComponent('ui-abstract-map', {});
 
@@ -9,10 +6,21 @@ test('it renders', function (assert) {
   assert.expect(2);
 
   // creates the component instance
-  var component = this.subject();
+  const component = this.subject();
   assert.equal(component._state, 'preRender');
 
   // renders the component to the page
   this.render();
   assert.equal(component._state, 'inDOM');
+});
+
+test('sets own map facade when not present', function (assert) {
+  assert.expect(1);
+
+  const component = this.subject();
+
+  // renders the component to the page
+  this.render();
+
+  assert.ok(component.get('mapFacade'), 'should have an instance of MapFacade');
 });
