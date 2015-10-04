629700b002d35bccde72eaba7aec42c1879f0c38
Kenneth Buck <kenneth@nestedsphere.com>
2015-10-04 13:28:45 -0400

    fixed jshint

--- addon/components/ui-test-map.js
+++ addon/components/ui-test-map.js
@@ -1,7 +1,6 @@
-import Ember from 'ember';
-import UIAbstractMap from './ui-abstract-map';
 
 import layout from '../templates/components/ui-test-map';
+import UIAbstractMap from './ui-abstract-map';
 
 export default UIAbstractMap.extend({
   layout: layout,
