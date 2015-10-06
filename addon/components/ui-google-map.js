3fc8b587ee156bec86278e1ebf0c834c8296b147
Kenneth Buck <kenneth@nestedsphere.com>
2015-10-06 07:48:19 -0400

    removed google utility

--- addon/components/ui-google-map.js
+++ addon/components/ui-google-map.js
@@ -1,7 +1,8 @@
-import GoogleUtiltity from '../utilities/google';
 import UIAbstractMap from './ui-abstract-map';
-
+import Ember from 'ember';
 import layout from '../templates/components/ui-google-map';
+
+const {assert} = Ember;
 
 /*global JSON*/
 
@@ -39,15 +40,13 @@ export default UIAbstractMap.extend({
   },
 
   getMapType() {
-    return GoogleUtiltity.encodeMapType(this.get('map').getMapTypeId());
+    return this.encodeMapType(this.get('map').getMapTypeId());
   },
 
   setMapType(value) {
-    let type = GoogleUtiltity.map.decodeMapType(value);
-
     const map = this.get('map');
 
-    map.setMapTypeId(type);
+    map.setMapTypeId(this.decodeMapType(value));
 
     return map.getMapTypeId();
   },
@@ -102,8 +101,8 @@ export default UIAbstractMap.extend({
   },
 
   addListener(eventName) {
-    const decodedEventName = GoogleUtiltity.map.decodeEventName(eventName);
-    const encodedEventAction = GoogleUtiltity.map.encodeEventAction(decodedEventName);
+    const decodedEventName = this.decodeEventName(eventName);
+    const encodedEventAction = this.encodeMapEventAction(decodedEventName);
 
     const {mapApi, map} = this.getProperties('mapApi', 'map');
 
@@ -156,8 +155,8 @@ export default UIAbstractMap.extend({
   },
 
   addMarkerListener(id, eventName) {
-    const decodedEventName = GoogleUtiltity.marker.decodeEventName(eventName);
-    const encodedEventAction = GoogleUtiltity.marker.encodeEventAction(decodedEventName);
+    const decodedEventName = this.decodeEventName(eventName);
+    const encodedEventAction = this.encodeMarkerEventAction(decodedEventName);
 
     const data = {id: id, type: 'marker'};
     const mapApi = this.get('mapApi');
@@ -252,6 +251,151 @@ export default UIAbstractMap.extend({
     });
 
     map.fitBounds(bounds);
+  },
+
+  decodeEventName(eventName) {
+    const alias = {
+      click: 'click',
+      animationChanged: 'animation_changed',
+      clickableChanged: 'clickable_changed',
+      cursorChanged: 'cursorChanged',
+      doubleClick: 'dblclick',
+      drag: 'drag',
+      dragEnd: 'dragend',
+      draggableChanged: 'draggable_changed',
+      dragStart: 'dragstart',
+      flatChanged: 'flat_changed',
+      iconChanged: 'icon_changed',
+      mouseDown: 'mousedown',
+      mouseOut: 'mouseout',
+      mouseOver: 'mouseover',
+      mouseUp: 'mouseup',
+      positionChanged: 'position_changed',
+      rightClick: 'rightclick',
+      shapeChanged: 'shape_changed',
+      titleChanged: 'title_changed',
+      visibleChanged: 'visible_changed',
+      zIndexChanged: 'zindex_changed',
+      boundsChanged: 'bounds_changed',
+      centerChanged: 'center_changed',
+      tilesLoaded: 'tilesloaded',
+      zoomChanged: 'zoom_changed',
+      headingChanged: 'heading_changed',
+      idle: 'idle',
+      mapTypeIdChanged: 'maptypeid_changed',
+      projectionChanged: 'projection_changed',
+      mouseMove: 'mousemove'
+    }[eventName];
+
+    assert(`there is no event mapping for the following marker event name: ${eventName}`, !!alias);
+
+    return alias;
+  },
+
+  decodeMapType(type) {
+    const google = this.get('mapApi');
+    let mappedType;
+
+    switch (type) {
+      case "roadmap":
+        mappedType = google.maps.MapTypeId.ROADMAP;
+        break;
+      case "satellite":
+        mappedType = google.maps.MapTypeId.SATELLITE;
+        break;
+      case "terrain":
+        mappedType = google.maps.MapTypeId.TERRAIN;
+        break;
+      case "hybrid":
+        mappedType = google.maps.MapTypeId.HYBRID;
+        break;
+      default:
+        mappedType = google.maps.MapTypeId.ROADMAP;
+    }
+
+    return mappedType;
+  },
+
+  encodeMapType(type) {
+    const google = this.get('mapApi');
+    let mappedType;
+
+    switch (type) {
+      case google.maps.MapTypeId.ROADMAP:
+        mappedType = "roadmap";
+        break;
+      case google.maps.MapTypeId.SATELLITE:
+        mappedType = "satellite";
+        break;
+      case google.maps.MapTypeId.TERRAIN:
+        mappedType = "terrain";
+        break;
+      case google.maps.MapTypeId.HYBRID:
+        mappedType = "hybrid";
+        break;
+      default:
+        mappedType = "roadmap";
+    }
+
+    return mappedType;
+  },
+
+  encodeMapEventAction(eventName) {
+    const alias = {
+      click: 'mapClickAction',
+      bounds_changed: 'mapBoundsChangedAction',
+      center_changed: 'mapCenterChangedAction',
+      dblclick: 'mapDoubleClickAction',
+      rightclick: 'mapRightClickAction',
+      tilesloaded: 'mapTilesLoadedAction',
+      zoom_changed: 'mapZoomChangedAction',
+      drag: 'mapDragAction',
+      dragend: 'mapDragEndAction',
+      dragstart: 'mapDragStartAction',
+      heading_changed: 'mapHeadingChangedAction',
+      idle: 'mapIdleAction',
+      maptypeid_changed: 'mapTypeIdChangedAction',
+      projection_changed: 'mapProjectionChangedAction',
+      mousemove: 'mapMouseMoveAction',
+      mouseup: 'mapMouseUpAction',
+      mousedown: 'mapMouseDownAction',
+      mouseover: 'mapMouseOverAction',
+      mouseout: 'mapMouseOutAction'
+    }[eventName];
+
+    assert(`there is no action mapping for the following map event name: ${eventName}`, !!alias);
+
+    return alias;
+  },
+
+  encodeMarkerEventAction(eventName) {
+    const alias = {
+      'animation_changed': 'markerAnimationChangedAction',
+      'click': 'markerClickAction',
+      'clickable_changed': 'markerClickableChangedAction',
+      'cursor_changed': 'markerCursorChangedAction',
+      'dblclick': 'markerDoubleClickAction',
+      'drag': 'markerDragAction',
+      'dragend': 'markerDragEndAction',
+      'draggable_changed': 'markerDraggableChangedAction',
+      'dragstart': 'markerDragStartAction',
+      'flat_changed': 'markerFlatChangedAction',
+      'icon_changed': 'markerIconChangedAction',
+      'mousedown': 'markerMouseDownAction',
+      'mouseout': 'markerMouseOutAction',
+      'mouseover': 'markerMouseOverAction',
+      'mouseup': 'markerMouseUpAction',
+      'position_changed': 'markerPositionChangedAction',
+      'rightclick': 'markerRightClickAction',
+      'shape_changed': 'markerShapeChangedAction',
+      'title_changed': 'markerTitleChangedAction',
+      'visible_changed': 'markerVisibleChangedAction',
+      'zindex_changed': 'markerZIndexChangedAction'
+    }[eventName];
+
+    assert(`there is no action mapping for the following event name: ${eventName}`, !!alias);
+
+    return alias;
   }
 });
 
