49f82548c481191a3ec235e77892748b74da9610
Kenneth Buck <kenneth@nestedsphere.com>
2015-10-18 19:13:32 -0400

    additional map event configuration tests

--- tests/integration/components/ui-google-map-test.js
+++ tests/integration/components/ui-google-map-test.js
@@ -7,7 +7,7 @@ moduleForComponent('ui-google-map', 'Integration | Component | ui google map', {
   integration: true
 });
 
-test('sends mapReadyAction on successful initialization', function (assert) {
+test('sends map ready action on successful initialization', function (assert) {
   assert.expect(1);
 
   this.setProperties({
@@ -46,31 +46,6 @@ test('sends configured map click action', function (assert) {
     start();
 
     map.triggerMapEvent('click', {lat: 0, lng: 0});
-  });
-});
-
-test('sends configured map right click action', function (assert) {
-  assert.expect(1);
-
-  this.setProperties({
-    markers: [],
-    mapApi: google,
-    config: {
-      mapEvents: ['rightClick']
-    }
-  });
-
-  this.render(hbs`{{ui-google-map markers=markers mapApi=mapApi config=config}}`);
-
-  this.on('mapRightClick', function () {
-    assert.ok(true, 'should send marker right click');
-  });
-
-  stop();
-  this.on('mapReady', (map) => {
-    start();
-
-    map.triggerMapEvent('rightClick', {lat: 0, lng: 0});
   });
 });
 
@@ -136,7 +111,7 @@ test('sends configured map double click action', function (assert) {
   this.render(hbs`{{ui-google-map markers=markers mapApi=mapApi config=config}}`);
 
   this.on('mapDoubleClick', function () {
-    assert.ok(true, 'should send center changed');
+    assert.ok(true, 'should map double click');
   });
 
   stop();
@@ -147,3 +122,125 @@ test('sends configured map double click action', function (assert) {
   });
 });
 
+test('sends configured map right click action', function (assert) {
+  assert.expect(1);
+
+  this.setProperties({
+    markers: [],
+    mapApi: google,
+    config: {
+      mapEvents: ['rightClick']
+    }
+  });
+
+  this.render(hbs`{{ui-google-map markers=markers mapApi=mapApi config=config}}`);
+
+  this.on('mapRightClick', function () {
+    assert.ok(true, 'should send marker right click');
+  });
+
+  stop();
+  this.on('mapReady', (map) => {
+    start();
+
+    map.triggerMapEvent('rightClick', {lat: 0, lng: 0});
+  });
+});
+
+test('sends configured map tiles loaded action', function (assert) {
+  assert.expect(1);
+
+  this.setProperties({
+    markers: [],
+    mapApi: google,
+    config: {
+      mapEvents: ['tilesLoaded']
+    }
+  });
+
+  this.render(hbs`{{ui-google-map markers=markers mapApi=mapApi config=config}}`);
+
+  this.on('mapTilesLoaded', function () {
+    assert.ok(true, 'should send map tiles loaded');
+  });
+
+  stop();
+  this.on('mapReady', () => {
+    start();
+  });
+});
+
+test('sends configured map zoom changed action', function (assert) {
+  assert.expect(1);
+
+  this.setProperties({
+    markers: [],
+    mapApi: google,
+    config: {
+      mapEvents: ['zoomChanged']
+    }
+  });
+
+  this.render(hbs`{{ui-google-map markers=markers mapApi=mapApi config=config}}`);
+
+  this.on('mapZoomChanged', function () {
+    assert.ok(true, 'should send map zoom changed');
+  });
+
+  stop();
+  this.on('mapReady', (map) => {
+    start();
+
+    map.triggerMapEvent('zoomChanged', {lat: 0, lng: 0});
+  });
+});
+
+test('sends configured map drag action', function (assert) {
+  assert.expect(1);
+
+  this.setProperties({
+    markers: [],
+    mapApi: google,
+    config: {
+      mapEvents: ['drag']
+    }
+  });
+
+  this.render(hbs`{{ui-google-map markers=markers mapApi=mapApi config=config}}`);
+
+  this.on('mapDrag', function () {
+    assert.ok(true, 'should send map drag');
+  });
+
+  stop();
+  this.on('mapReady', (map) => {
+    start();
+
+    map.triggerMapEvent('drag', {lat: 0, lng: 0});
+  });
+});
+
+test('sends configured map drag start action', function (assert) {
+  assert.expect(1);
+
+  this.setProperties({
+    markers: [],
+    mapApi: google,
+    config: {
+      mapEvents: ['dragStart']
+    }
+  });
+
+  this.render(hbs`{{ui-google-map markers=markers mapApi=mapApi config=config}}`);
+
+  this.on('mapDragStart', function () {
+    assert.ok(true, 'should send map drag start');
+  });
+
+  stop();
+  this.on('mapReady', (map) => {
+    start();
+
+    map.triggerMapEvent('dragStart', {lat: 0, lng: 0});
+  });
+});
