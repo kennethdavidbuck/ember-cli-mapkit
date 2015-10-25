6d94bd218d15a0253db11955a5d5872435499eea
Kenneth Buck <kenneth@nestedsphere.com>
2015-10-25 15:55:28 -0400

    updated config to include gh-pages environment

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -47,8 +47,12 @@ module.exports = function(environment) {
     ENV.APP.rootElement = '#ember-testing';
   }
 
-  if (environment === 'production') {
+  if (environment === 'gh-pages') {
     ENV.baseURL = '/ember-cli-mapkit';
+  }
+
+  if (environment === 'production') {
+    ENV.baseURL = '/';
   }
 
   return ENV;
